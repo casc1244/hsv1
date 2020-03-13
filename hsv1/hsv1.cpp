da3019da3ec5fbfda64ca92a78e7cb18a9635949
casc1244 <1356602646@qq.com>
2020-03-13 14:28:59 +0800

    hsv1.cpp

--- hsv1/hsv1.cpp
+++ hsv1/hsv1.cpp
@@ -1,20 +1,43 @@
-﻿// hsv1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <opencv2/opencv.hpp>
 #include <iostream>
+using namespace cv;
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{VideoCapture cap(0);
+	double scale = 1.5;
+	//设定皮肤的颜色
+	double i_minh = 0;
+	double i_maxh = 20;
+	//设定饱和度
+	double i_mins = 43;
+	double i_maxs = 255;
+	//设定亮度
+	double i_minv = 100;
+	double i_maxv = 255;
+
+	while (1)
+	{
+		Mat frame;
+		Mat hsvMat;
+		Mat detectMat;
+
+		cap >> frame;
+		Size resimgsize = Size(frame.cols*scale, frame.rows*scale);
+		Mat rFrame = Mat(resimgsize, frame.type());
+		resize(frame, rFrame, resimgsize, INTER_LINEAR);
+
+		cvtColor(rFrame, hsvMat, COLOR_BGR2HSV);
+
+		rFrame.copyTo(detectMat);
+		inRange(hsvMat, Scalar(i_minh, i_mins, i_minv), Scalar(i_maxh, i_maxs, i_maxv), detectMat);
+		imshow("while :in the range", detectMat);
+		imshow("frame", rFrame);
+		waitKey(30);
+	}
+
+	return 0;
+
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
